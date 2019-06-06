b9579bad6a9479b77b40a77277502a1960133ece
Cameron <cameron.stalker@digital.hmrc.gov.uk>
2019-06-06 14:49:53 +0100

    Added some comments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''
+Project: Calculator
+Author: Cameron 'the' Stalker
+
+'''
+
 import re
 
 print("Magical Calculator")
@@ -11,10 +17,14 @@ def performMath():
     global run
     global previous
     equation = ""
+
+    # if there is a previous calculation, use this in the prompt
     if previous == 0:
         equation = input("Enter equation:\n")
     else:
         equation = input(str(previous))
+
+    # if the user quits
     if equation == "q":
         print("Goodbye")
         run = False
